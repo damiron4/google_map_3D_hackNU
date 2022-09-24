4d4c57630ad15121c670f53312bdf7443b643973
Tamerlan-K <tamerlan.khussainov@nu.edu.kz>
2022-09-25 05:58:36 +0600

    added mapOptions for each altitude

--- starter/src/app.js
+++ starter/src/app.js
@@ -20,58 +20,135 @@ const apiOptions = {
   "apiKey": "AIzaSyAuB2wpky1mqRPWuFUjxmmfW2ZeLzYgSeo",
   "version": "beta"
 };
-var markersArray = [];
-var fixPtr = 0;
-const maxSteps = 200;
-var step = 0;
 
+function getFixesByName(name, data) {
+  const fixes = [];
+  for (const fix of data) {
+    if (fix.Identifier == name) fixes.push(fix);
+  }
+  return fixes;
+}
+
+function recalc() {
+  LatStep =  (selectedUserFixes[(fixPtr + 1) % selectedUserFixes.length]["Latitude"] - selectedUserFixes[fixPtr]["Latitude"])/maxSteps;
+  LngStep =  (selectedUserFixes[(fixPtr + 1) % selectedUserFixes.length]["Longitude"] - selectedUserFixes[fixPtr]["Longitude"])/maxSteps;
+
+  horizontalAccuracy = selectedUserFixes[fixPtr]["Horizontal accuracy"] / confidenceInAccuracy;
+  verticalAccuracy = selectedUserFixes[fixPtr]["Vertical accuracy"] / confidenceInAccuracy;
+}
+
+function setMapOptions(height) {
+  if (height >= 86000) {
+    mapOptions.tilt = 0;
+    mapOptions.zoom = 10;
+    return;
+  }
+  if (height >= 43000) {
+    mapOptions.tilt = 0;
+    mapOptions.zoom = 11;
+    return;
+  }
+  if (height >= 21500) {
+    mapOptions.tilt = 0;
+    mapOptions.zoom = 12;
+    return;
+  }
+  if (height >= 10500) {
+    mapOptions.tilt = 0;
+    mapOptions.zoom = 13;
+    return;
+  }
+  if (height >= 3000) {
+    mapOptions.tilt = 0;
+    mapOptions.zoom = 14;
+    return;
+  }
+  if (height >= 1900) {
+    mapOptions.tilt = 30;
+    mapOptions.zoom = 14;
+    return;
+  }
+  if (height >= 500) {
+    mapOptions.tilt = 40;
+    mapOptions.zoom = 15;
+    return;
+  }
+  if (height >= 300) {
+    mapOptions.tilt = 50;
+    mapOptions.zoom = 16;
+    return;
+  }
+  if (height >= 200) {
+    mapOptions.tilt = 60;
+    mapOptions.zoom = 17;
+    return;
+  }
+  if (height >= 120) {
+    mapOptions.tilt = 70;
+    mapOptions.zoom = 18;
+    return;
+  }
+  if (height >= 60) {
+    mapOptions.tilt = 90;
+    mapOptions.zoom = 19;
+    return;
+  }
+  if (height >= 40) {
+    mapOptions.tilt = 90;
+    mapOptions.zoom = 20;
+    return;
+  }
+  if (height >= 20) {
+    mapOptions.tilt = 90;
+    mapOptions.zoom = 21;
+    return;
+  }
+  mapOptions.tilt = 20;
+  mapOptions.zoom = 22;
+  return;
+}
+
+var fixPtr = 0;
+var step = 0;
+const maxSteps = 100;
 const confidenceInAccuracy = 0.6827;
 
 import data from './dataset/dev10.json';
 
-// Extract the selected user's fixes
 var selectedUserFixes = [];
-const path = [];
-for (const fix of data) {
-  if(fix.Identifier == 'Tenzing')
-  selectedUserFixes.push(fix);
-  path.push({lat: fix.Latitude, lng: fix.Longitude});
+// Extract the selected user's fixes
+if (data.length > 1) {
+  selectedUserFixes = getFixesByName(data[0]["Identifier"], data)
+} else {
+  selectedUserFixes = data;
 }
-// fetch('./dataset/dev5.json')
-//     .then(response => response.json())
-//     .then(json => console.log(json));
 
 var horizontalAccuracy = selectedUserFixes[0]["Horizontal accuracy"];
 var verticalAccuracy = selectedUserFixes[0]["Vertical accuracy"];
-var LatStep =  (selectedUserFixes[1]["Latitude"] - selectedUserFixes[0]["Latitude"])/maxSteps;
-var LngStep =  (selectedUserFixes[1]["Longitude"] - selectedUserFixes[0]["Longitude"])/maxSteps;
-var AltStep = 0.000001;
+var LatStep, LngStep;
+if (data.length > 1) {
+  horizontalAccuracy = selectedUserFixes[0]["Horizontal accuracy"];
+  verticalAccuracy = selectedUserFixes[0]["Vertical accuracy"];
+  LatStep =  (selectedUserFixes[1]["Latitude"] - selectedUserFixes[0]["Latitude"])/maxSteps;
+  LngStep =  (selectedUserFixes[1]["Longitude"] - selectedUserFixes[0]["Longitude"])/maxSteps;
+}
 
 const mapOptions = {
   "tilt": 0,
-  "heading": 30, 
-  "zoom": 14,
+  "heading": 40, 
+  "zoom": 18,
   "center": { lat: selectedUserFixes[0]["Latitude"], lng: selectedUserFixes[0]["Longitude"], altitude: selectedUserFixes[0]["Altitude"]},
   "mapId": "a9c41552dfc27a5"
 }
+
+setMapOptions(data[0]["Altitude"]);
 
 async function initMap() {
   const mapDiv = document.getElementById("map");
   const apiLoader = new Loader(apiOptions);
   await apiLoader.load();
-  const map = new google.maps.Map(mapDiv, mapOptions);
 
-  // const path = new google.maps.Polyline({
-  //   path: path,
-  //   geodesic: true,
-  //   strokeColor: "#0000FF",
-  //   strokeOpacity: 0.4,
-  //   strokeWeight: 2
-  // });
-
-  // path.setMap(map);
-
-  return map;
+  return new google.maps.Map(mapDiv, mapOptions);
 }
 
 async function initWebGLOverlayView (map) {
@@ -85,7 +162,7 @@ const webGLOverlayView = new google.maps.WebGLOverlayView();
 webGLOverlayView.onAdd = () => {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera();
-  camera.lookAt(scene.position);
+  // camera.lookAt(scene.position);
   const ambientLight = new THREE.AmbientLight( 0xffffff, 0.75 ); // soft white light
   scene.add( ambientLight );
   const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
@@ -97,23 +174,24 @@ webGLOverlayView.onAdd = () => {
   const sphere_transparent = 'sphere-transparent.gltf';
   const scaleHorizontal = horizontalAccuracy/confidenceInAccuracy;
   const scaleVertical = verticalAccuracy/confidenceInAccuracy;
+  
+  // Load small sphere
+  loader.load(
+    sphere,
+    gltf => {
+      gltf.scene.scale.set(0.5 * scaleVertical, 0.5 * scaleVertical, 0.5 * scaleVertical);
+      gltf.scene.translateZ(scaleVertical*0.375);
+    
+      scene.add(gltf.scene);
+    }
+  );
+
   // Load transparent sphere
   loader.load(
     sphere_transparent,
     gltf => {
       gltf.scene.scale.set(0.0005 * scaleHorizontal, 0.0005 * scaleHorizontal, 0.0005 * scaleVertical);
       gltf.scene.translateZ(scaleVertical*0.5);
-      scene.add(gltf.scene);
-    }
-  );
-  
-  // Load small sphere
-  loader.load(
-    sphere,
-    gltf => {
-      gltf.scene.scale.set(0.5 * scaleHorizontal, 0.5 * scaleHorizontal, 0.5 * scaleVertical);
-      gltf.scene.translateZ(scaleVertical*0.375);
-    
       scene.add(gltf.scene);
     }
   );
@@ -130,26 +208,31 @@ webGLOverlayView.onContextRestored = ({gl}) => {
 
   loader.manager.onLoad = () => {
     renderer.setAnimationLoop(() => {
+      if (selectedUserFixes.length < 2) {
+        //renderer.setAnimationLoop(null);
+        return;
+      }
       map.moveCamera({
-        "tilt": mapOptions.tilt,
-        "heading": mapOptions.heading,
+        // "tilt": mapOptions.tilt,
+        // "heading": mapOptions.heading,
         // "zoom": mapOptions.zoom,
         "center": {lat: mapOptions.center.lat, lng: mapOptions.center.lng}
       });
 
-      if (mapOptions.tilt < 0) {
-        mapOptions.tilt += 0.5;
-      } else {
-        mapOptions.center.lat += LatStep;
-        mapOptions.center.lng += LngStep;
-        step++;
-        if(step >= maxSteps) {
-          fixPtr = (fixPtr + 1) % selectedUserFixes.length;
-          step = 0;
-          LatStep =  (selectedUserFixes[(fixPtr + 1) % selectedUserFixes.length]["Latitude"] - selectedUserFixes[fixPtr]["Latitude"])/maxSteps;
-          LngStep =  (selectedUserFixes[(fixPtr + 1) % selectedUserFixes.length]["Longitude"] - selectedUserFixes[fixPtr]["Longitude"])/maxSteps;
-      }}
-     } );
+      mapOptions.center.lat += LatStep;
+      mapOptions.center.lng += LngStep;
+      step++;
+      if(step >= maxSteps) {
+        fixPtr++;
+        if(fixPtr >= selectedUserFixes.length) {
+          mapOptions.center.lat = selectedUserFixes[0]["Latitude"];
+          mapOptions.center.lng = selectedUserFixes[0]["Longitude"];
+          fixPtr = 0;
+        }
+        step = 0;
+        recalc();
+      }
+     });
   }
 }
 
